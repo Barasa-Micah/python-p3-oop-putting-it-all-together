e043cc2b9c5ef4c0ade7b6978bb667641f3fe0c1
Barasa-Micah <barasamica@gmail.com>
2023-08-24 14:52:15 +0300

    PAssed these tests

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand=brand
+        self.size= size
+        self.condition= 'used'  #default condition
+
+
+    def int_size(self):
+        if  not isinstance(self.size, int):
+            print('size must be an integer')
+
+
+
+    def can_cobble(self):
+        self.condition = 'New'
+        print('Your shoe is as good as new! \n')            
+
+
+
+
+    condition = ('Nike', 10)
+    condition.cobble = 'New'    
